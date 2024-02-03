9abc07eb3a5c0049261046be42ed9be87c7c90e1
René Kulik <rene@kulik.io>
2024-02-03 21:38:36 +0100

    feat: load route plugins automatically

--- src/setup/build-server.ts
+++ src/setup/build-server.ts
@@ -1,7 +1,8 @@
+import autload from '@fastify/autoload';
 import fastify, { FastifyInstance } from 'fastify';
 
+import path from 'path';
+
 export const buildServer = (): FastifyInstance => {
-  return fastify().get('/ping', () => {
-    return 'pong\n';
-  });
+  return fastify().register(autload, { dir: path.join(__dirname, '..', 'routes'), options: { prefix: '/api/v1' } });
 };
