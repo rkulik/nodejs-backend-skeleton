156dd194ab3458cb2f653e074001e27dcd02e4fa
René Kulik <rene@kulik.io>
2024-02-03 22:12:56 +0100

    feat: load environment configurations

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: process.env.NODE_ENV ? [`.env.${process.env.NODE_ENV}`, '.env'] : '.env' });
+
+export const config = {
+  server: {
+    port: Number(process.env.PORT!),
+    host: process.env.HOST!,
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import { config } from './config';
 import { buildServer } from './setup/build-server';
 import { startServer } from './setup/start-server';
 
 const main = async (): Promise<void> => {
-  await startServer(buildServer(), { port: 8080 });
+  const {
+    server: { port, host },
+  } = config;
+
+  await startServer(buildServer(), { port, host });
 };
 
 main();
