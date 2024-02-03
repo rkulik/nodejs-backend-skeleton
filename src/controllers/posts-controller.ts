4912292e7c09d59e5a1984675dbc0e3cbdc012d4
René Kulik <rene@kulik.io>
2024-02-03 23:20:58 +0100

    feat: create posts controller and routes

---
+++ src/controllers/posts-controller.ts
@@ -0,0 +1,62 @@
+type Post = {
+  id: number;
+  title: string;
+  content: string;
+};
+
+export type CreatePostDto = Omit<Post, 'id'>;
+
+export type UpdatePostDto = Omit<Post, 'id'>;
+
+const posts: Post[] = [
+  {
+    id: 1,
+    title: 'First post',
+    content: 'The content of the first post',
+  },
+  {
+    id: 2,
+    title: 'Second post',
+    content: 'The content of the second post',
+  },
+];
+
+export class PostsController {
+  public read(): Post[] {
+    return posts;
+  }
+
+  public readOne(id: number): Post | undefined {
+    return posts.find((post) => {
+      return post.id === id;
+    });
+  }
+
+  public create(createPostDto: CreatePostDto): Post {
+    return {
+      id: 3,
+      ...createPostDto,
+    };
+  }
+
+  public update(id: number, updatePostDto: UpdatePostDto): Post | undefined {
+    const existingPost = posts.find((post) => {
+      return post.id === id;
+    });
+
+    return existingPost
+      ? {
+          ...existingPost,
+          ...updatePostDto,
+        }
+      : undefined;
+  }
+
+  public delete(id: number): boolean {
+    return posts
+      .map((post) => {
+        return post.id;
+      })
+      .includes(id);
+  }
+}
---
+++ src/routes/posts.ts
@@ -0,0 +1,35 @@
+import { FastifyPluginCallback } from 'fastify';
+
+import { CreatePostDto, PostsController, UpdatePostDto } from '../controllers/posts-controller';
+
+const posts: FastifyPluginCallback = (server, _options, done) => {
+  const postsController = new PostsController();
+
+  server.get('/posts', (_request, reply) => {
+    reply.send(postsController.read());
+  });
+
+  server.get<{ Params: { id: string } }>('/posts/:id', (request, reply) => {
+    const post = postsController.readOne(Number(request.params.id));
+    post ? reply.send(post) : reply.code(404).send({ error: { message: 'Not found' } });
+  });
+
+  server.post<{ Body: CreatePostDto }>('/posts', (request, reply) => {
+    reply.send(postsController.create(request.body));
+  });
+
+  server.put<{ Params: { id: string }; Body: UpdatePostDto }>('/posts/:id', (request, reply) => {
+    const updatedPost = postsController.update(Number(request.params.id), request.body);
+    updatedPost ? reply.send(updatedPost) : reply.code(404).send({ error: { message: 'Not found' } });
+  });
+
+  server.delete<{ Params: { id: string } }>('/posts/:id', (request, reply) => {
+    const isDeleted = postsController.delete(Number(request.params.id));
+    isDeleted ? reply.code(204).send() : reply.code(404).send({ error: { message: 'Not found' } });
+  });
+
+  done();
+};
+
+// eslint-disable-next-line import/no-default-export
+export default posts;
