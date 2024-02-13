a3920aa85ae4f9c8cd97f76db34a7293b5454448
René Kulik <rene@kulik.io>
2024-02-13 22:45:01 +0100

    refactor: return more meaningful not found messages

--- src/modules/comments/routes.ts
+++ src/modules/comments/routes.ts
@@ -13,29 +13,29 @@ const comments: FastifyPluginCallbackJsonSchemaToTs = (server, _options, done) =
 
   server.post('/comments', createCommentSchema, (request, reply) => {
     const comment = commentsController.create(request.body);
-    comment ? reply.sendSuccess({ comment }) : reply.code(404).sendFail({ message: 'Not found' });
+    comment ? reply.sendSuccess({ comment }) : reply.code(404).sendFail({ message: 'Post not found' });
   });
 
   server.get('/posts/:id/comments', getPostCommentsSchema, (request, reply) => {
     const comments = postCommentsController.read(request.params.id);
-    comments ? reply.sendSuccess({ comments }) : reply.code(404).sendFail({ message: 'Not found' });
+    comments ? reply.sendSuccess({ comments }) : reply.code(404).sendFail({ message: 'Post not found' });
   });
 
   server.get('/comments/:id', getCommentSchema, (request, reply) => {
     const comment = commentsController.readOne(request.params.id);
-    comment ? reply.sendSuccess({ comment }) : reply.code(404).sendFail({ message: 'Not found' });
+    comment ? reply.sendSuccess({ comment }) : reply.code(404).sendFail({ message: 'Comment not found' });
   });
 
   server.put('/comments/:id', updateCommentSchema, (request, reply) => {
     const updatedComment = commentsController.update(request.params.id, request.body);
     updatedComment
       ? reply.sendSuccess({ comment: updatedComment })
-      : reply.code(404).sendFail({ message: 'Not found' });
+      : reply.code(404).sendFail({ message: 'Comment not found' });
   });
 
   server.delete('/comments/:id', deleteCommentSchema, (request, reply) => {
     const isDeleted = commentsController.delete(request.params.id);
-    isDeleted ? reply.code(204).send() : reply.code(404).sendFail({ message: 'Not found' });
+    isDeleted ? reply.code(204).send() : reply.code(404).sendFail({ message: 'Comment not found' });
   });
 
   done();
--- src/modules/posts/routes.ts
+++ src/modules/posts/routes.ts
@@ -23,27 +23,31 @@ const posts: FastifyPluginCallbackJsonSchemaToTs = (server, _options, done) => {
 
   server.get('/posts/:id', getPostSchema, (request, reply) => {
     const post = postsController.readOne(request.params.id);
-    post ? reply.sendSuccess({ post }) : reply.code(404).sendFail({ message: 'Not found' });
+    post ? reply.sendSuccess({ post }) : reply.code(404).sendFail({ message: 'Post not found' });
   });
 
   server.put('/posts/:id', updatePostSchema, (request, reply) => {
     const updatedPost = postsController.update(request.params.id, request.body);
-    updatedPost ? reply.sendSuccess({ post: updatedPost }) : reply.code(404).sendFail({ message: 'Not found' });
+    updatedPost ? reply.sendSuccess({ post: updatedPost }) : reply.code(404).sendFail({ message: 'Post not found' });
   });
 
   server.delete('/posts/:id', deletePostSchema, (request, reply) => {
     const isDeleted = postsController.delete(request.params.id);
-    isDeleted ? reply.code(204).send() : reply.code(404).sendFail({ message: 'Not found' });
+    isDeleted ? reply.code(204).send() : reply.code(404).sendFail({ message: 'Post not found' });
   });
 
   server.post('/published-posts', publishPostSchema, (request, reply) => {
     const publishedPost = publishedPostsController.create(request.body);
-    publishedPost ? reply.sendSuccess({ post: publishedPost }) : reply.code(404).sendFail({ message: 'Not found' });
+    publishedPost
+      ? reply.sendSuccess({ post: publishedPost })
+      : reply.code(404).sendFail({ message: 'Post not found' });
   });
 
   server.delete('/published-posts/:id', unpublishPostSchema, (request, reply) => {
     const unpublishedPost = publishedPostsController.delete(request.params.id);
-    unpublishedPost ? reply.sendSuccess({ post: unpublishedPost }) : reply.code(404).sendFail({ message: 'Not found' });
+    unpublishedPost
+      ? reply.sendSuccess({ post: unpublishedPost })
+      : reply.code(404).sendFail({ message: 'Post not found' });
   });
 
   done();
